b4f573d82c428c3097c45d52526e79b2fc2b3b7b
Andrii Zadorozhnii <zador746@gmail.com>
2024-05-30 16:50:20 +0300

    lesson1

---
+++ Lesson1/lesson1.py
@@ -0,0 +1,31 @@
+# Print This is fun!
+print("This is fun")
+
+#   Training
+#   Type 2 lines of code and play the code
+#   Line 1: Create comments that says This code print Hello World
+#   Line 2: Print Hello World
+
+# This code print Hello World
+print('Hello World')
+
+# str() int() bool() float() print() abs() dir() input() max() main() round() sum()
+
+#   string()
+#   This code defines a name and then print this
+name = str("Mr. Skywalker") # We named Mr.Look
+age = int(44)
+
+print(name)
+
+print(type(name))
+print(type(age))
+
+#   Training
+
+
+name2 = str("Dart Vader")
+print(name2)
+age2 = int(19)
+print(age2)
+
