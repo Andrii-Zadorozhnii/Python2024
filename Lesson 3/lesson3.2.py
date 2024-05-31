44113de4c07342c0643fe5e04fc63eceb2cb2d42
Andrii Zadorozhnii <zador746@gmail.com>
2024-05-31 16:50:18 +0300

    lesson 4 In progress

---
+++ Lesson 3/lesson3.2.py
@@ -0,0 +1,13 @@
+#   Asoka is 1.70 meters, Yoda = 0.66, R2D2 = 1.09, C3P0 = 1.75m
+#   1: Get the average height of the 4 characters
+#   2: Print the average height
+#   3:  Define 4 variables in your calculation as follow:
+#       Asoka height, Yoda height, R2D2 height, C3P0 height
+
+asoka_height = float(1.70)
+yoda_height = float(0.66)
+r2d2_height = float(1.09)
+c3po_height = float(1.75)
+
+average_height = (asoka_height + yoda_height + r2d2_height + c3po_height)/4
+print("Average height: " + str(average_height) + "m")
