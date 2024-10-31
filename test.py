0f98f29b02681e09b4a37fe92fc53dcfe79be813
Andrii Zadorozhnii <zador746@gmail.com>
2024-10-31 21:51:26 +0200

    Lesson_1

---
+++ test.py
@@ -0,0 +1,17 @@
+import this
+
+name = "Andrii"
+age = 32
+love_color = "blue"
+best_dishes = ['pizza', 'burger', 'meat']
+
+
+print('my name is: ', name)
+print('my age is: ', age)
+print('my lovely color is: ', love_color)
+print('my lovely dishes are: ', best_dishes)
+
+me = {'name': name, 'age': age, 'lovely_colors': love_color}
+
+print(me)
+
